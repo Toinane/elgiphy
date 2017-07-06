3807c8f54faf0065d5f513bb732bcc3d74528638
Toinane <toinane@yahoo.fr>
2017-07-06 17:49:47 +0200

    initial state

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ const {app, BrowserWindow} = require('electron');
 
 app.on("ready", event => createBrowser());
 
+app.on("will-quit", event => console.log('bye-bye all!'));
+
 function createBrowser() {
 	const options = {
 		width: 350,
 		height: 600
 	}
 
-	let browser = new BrowserWindow(options);
+	let browsner = new BrowserWindow(options);
 	browser.openDevTools();
 	browser.loadURL(`file://${__dirname}/src/index.html`);
 }
