47a4937c577c93abffcfbe64f2a3a180253a12ec
Toinane <toinane@yahoo.fr>
2017-07-06 17:44:18 +0200

    initial state

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 'use strict';
 
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, globalShortcut, ipcMain} = require('electron');
 
-app.on("ready", event => createBrowser());
+app.on("ready", event => {
+	globalShortcut.register("CommandOrControl+Shift+Space", function(){});
+	createBrowser();
+});
+
+app.on("will-quit", event => {
+	globalShortcut.unregister("CommandOrControl+Shift+Space");
+});
+
+ipcMain.on('viewActive', (event, message) => {
+	console.log(message);
+});
 
 app.on("will-quit", event => console.log('bye-bye all!'));
 
 function createBrowser() {
 	const options = {
 		width: 350,
-		height: 600
+		height: 600,
+		frame: false,
+		resizable: false,
+		vibrancy: 'selection'
 	}
 
 	let browser = new BrowserWindow(options);
-	browser.openDevTools();
+	//browser.openDevTools();
 	browser.loadURL(`file://${__dirname}/src/index.html`);
 }
